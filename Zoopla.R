bb941a33e3da03e82228bca271252ca6ae33a25d
pythiantech <dhiraj@pythiantech.com>
2018-09-20 12:10:52 +0530

    Added postcodes

--- Zoopla.R
+++ Zoopla.R
@@ -1,8 +1,11 @@
+#Load the libraries
+
 library(tidyverse)
 library(leaflet)
 library(htmlwidgets)
 library(httr)
 library(xml2)
+library(XML)
 library(jsonlite)
 library(ggmap)
 library(leaflet.extras)
@@ -10,33 +13,73 @@ library(mapview)
 library(htmltools)
 library(qdapRegex)
 
-zkey <- "mwkjfa5y3emjy89tjtngqqxw"
-zkey <- "fr6vnwdte67xr6yeevpt979e" #Latest
-zkey <- "rs6br4vcc845e7qw6nvdqaxq"
-
+#API keys from Zoopla
+#Use any one of them at a time
 zkey <- "aca49hyxjbnpjzkean63r6ay"
-address <- "29 Richborne Terrace, London"
-location <- geocode(address)
+zkey <- "em8twa73q73zrryaytbrg4jr" #Utpal's key which is not yet activated
 
-proplistings <-  "https://api.zoopla.co.uk/api/v1/property_listings.json"
+#Read in the postcodes (source: https://data.london.gov.uk/dataset/mylondon)
+#Usefule site: https://postcodes.io/
+outcodes <- read_csv("data/MyLondon_postcode_OA.csv")
 
-pl <- GET(proplistings, query=list(api_key=zkey,
-                         latitude=51.479,
-                         longitude=-0.11964,
-                         radius=5
-                         ))
+#Get all the unique outcodes in a vector
+OC <- unique(outcodes$PC_DIST) #276 unique outcodes
 
 
-avgprice <- "http://api.zoopla.co.uk/api/v1/average_sold_prices.json"
+#########################################################################################
+#########################################################################################
+#Zoopla estimates is only available on request
 
-ap <- GET(avgprice, query=list(api_key=zkey,postcode='SW81AS',
-                               output_type='outcode',area_type='postcodes',
+#########################################################################################
+#########################################################################################
+#Average Sold Prices; this appears to be our best bet
+#Have a look at the details for this API: https://developer.zoopla.co.uk/docs/read/Average_Sold_Prices
+
+avgprice <- "http://api.zoopla.co.uk/api/v1/average_sold_prices.json" #You can convert the URL to return data in json format
+
+#Out of the 276 outcodes available, let's try it on the first one, OC[1], which is "BR1"
+#Use the GET() function from the httr package
+
+ap <- GET(avgprice, query=list(api_key=zkey,
+                               postcode=OC[1],
+                               output_type='outcode',
+                               area_type='streets',
                                page_number=1,
-                               page_size=20))
+                               page_size=20)) #page size can't be more than 20
 avgP <- content(ap, "text")
+json_content <- avgP %>% fromJSON() #Since the content is in json format
 
-json_content <- avgP %>% fromJSON()
-data <- json_content$areas
+#Currently we got the data for only one page. Let's find out how many number of 
+#pages will the API return
+
+results <- json_content$result_count #484 results! 
+#The API permits 100 calls per second and 100 calls per hour
+
+#So we'll have to run this in a loop and store the information in a dataframe.
+#I generally make an empty dataframe and then append the information there
+#Since we've already made a call once, let's have a look at the data returned
+
+data <- json_content$areas #It's a dataframe with 20 rows and 10 columns. Will append new data to this itself.
+
+for (i in 2:results%/%20){
+  ap <- GET(avgprice, query=list(api_key=zkey,
+                                 postcode=OC[i],
+                                 output_type='outcode',area_type='streets',
+                                 page_number=i,
+                                 page_size=20))
+  avgP <- content(ap, "text")
+  json_content <- avgP %>% fromJSON() 
+  
+  #And now bind the rows to our existing dataframe called data
+  data <- bind_rows(data, json_content$areas)
+}
+
+#
+#########################################################################################
+#########################################################################################
+
+
+
 icon.ion <- makeAwesomeIcon(icon = 'home', markerColor = 'green',
                             library='ion')
 
@@ -47,3 +90,20 @@ leaflet() %>% addTiles() %>% addRectangles(lng1 = as.numeric(json_content$boundi
                                           fillColor = 'transparent') %>%
   addAwesomeMarkers(lng = json_content$lon,lat=json_content$lat,icon=icon.ion)
 
+
+#################################################################################
+#Zed Index (Zed is Dead!) source: https://developer.zoopla.co.uk/docs/read/Zed_Index_API
+#The Zed-Index is the average property value in a given area based on current Zoopla Estimates
+#More information here: https://www.zoopla.co.uk/property/estimate/about/
+
+zurl <- "https://api.zoopla.co.uk/api/v1/zed_index"
+
+#Use the GET() function from httr package
+zed <- GET(zurl, query = list(api_key = zkey,
+                              area = OC[1],
+                              output_type = 'outcode'))
+
+zedContent <- content(zed, "text") 
+doc <- xmlParse(zedContent) #since the content is in XML format
+
+zedDF <- xmlToDataFrame(node=getNodeSet(doc, "/response"))#Convert it into a dataframe
